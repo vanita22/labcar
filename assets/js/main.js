7f608c1e6a2d7b270d12bfccca731a5c51d5f5db
vanita22 <inesvanessapcm@gmail.com>
2017-06-22 10:50:21 -0400

    subiendo el mapa

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,99 +1,45 @@
 function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          mapTypeControl: false,
-          center: {lat: -33.8688, lng: 151.2195},
-          zoom: 13
-        });
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 5,
+    center: {lat: -9.1191427, lng: -77.0349046},
+    mapTypeControl: false,
+    zoomControl: false,
+    streetViewControl: false
+  });  
+}
 
-        new AutocompleteDirectionsHandler(map);
+
+
+  var start = document.getElementById('origen');
+  var end = document.getElementById('destino');
+
+  new google.maps.places.Autocomplete(start);
+  new google.maps.places.Autocomplete(end);
+
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+
+  var calcularRuta = function(directionsService,directionsDisplay){
+    directionsService.route({
+    origin: start.value,
+    destination: end.value,
+    travelMode: 'DRIVING'
+    }, function (response,status){
+      if (status === 'OK') {
+          directionsDisplay.setDirections(response);
+        }else{
+      window.alert("No encontramos una ruta");
       }
+    })
+  };
 
-     
-      function AutocompleteDirectionsHandler(map) {
-        this.map = map;
-        this.originPlaceId = null;
-        this.destinationPlaceId = null;
-        this.travelMode = 'WALKING';
-        var originInput = document.getElementById('origin-input');
-        var destinationInput = document.getElementById('destination-input');
-        var modeSelector = document.getElementById('mode-selector');
-        this.directionsService = new google.maps.DirectionsService;
-        this.directionsDisplay = new google.maps.DirectionsRenderer;
-        this.directionsDisplay.setMap(map);
-
-        var originAutocomplete = new google.maps.places.Autocomplete(
-            originInput, {placeIdOnly: true});
-        var destinationAutocomplete = new google.maps.places.Autocomplete(
-            destinationInput, {placeIdOnly: true});
-
-        this.setupClickListener('changemode-walking', 'WALKING');
-        this.setupClickListener('changemode-transit', 'TRANSIT');
-        this.setupClickListener('changemode-driving', 'DRIVING');
-
-        this.setupPlaceChangedListener(originAutocomplete, 'ORIG');
-        this.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
-
-        this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
-        this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(destinationInput);
-        this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
-      }
-
-      // Sets a listener on a radio button to change the filter type on Places
-      // Autocomplete.
-      AutocompleteDirectionsHandler.prototype.setupClickListener = function(id, mode) {
-        var radioButton = document.getElementById(id);
-        var me = this;
-        radioButton.addEventListener('click', function() {
-          me.travelMode = mode;
-          me.route();
-        });
-      };
-
-      AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener = function(autocomplete, mode) {
-        var me = this;
-        autocomplete.bindTo('bounds', this.map);
-        autocomplete.addListener('place_changed', function() {
-          var place = autocomplete.getPlace();
-          if (!place.place_id) {
-            window.alert("Please select an option from the dropdown list.");
-            return;
-          }
-          if (mode === 'ORIG') {
-            me.originPlaceId = place.place_id;
-          } else {
-            me.destinationPlaceId = place.place_id;
-          }
-          me.route();
-        });
-
-      };
-
-      AutocompleteDirectionsHandler.prototype.route = function() {
-        if (!this.originPlaceId || !this.destinationPlaceId) {
-          return;
-        }
-        var me = this;
-
-        this.directionsService.route({
-          origin: {'placeId': this.originPlaceId},
-          destination: {'placeId': this.destinationPlaceId},
-          travelMode: this.travelMode
-        }, function(response, status) {
-          if (status === 'OK') {
-            me.directionsDisplay.setDirections(response);
-          } else {
-            window.alert('Directions request failed due to ' + status);
-          }
-        });
-
+    directionsDisplay.setMap(map);
+    var trazarRuta = function(){
+      calcularRuta(directionsService,directionsDisplay);
     };
 
-        function valor(){
-        	var result = document.getElementById("result");
+    document.getElementById("ruta").addEventListener('click', trazarRuta)
+  }
 
-        	var post = document.createElement("p");/*con este Dom se imprime por pantalla lo que se ingresa en el input en el nombre y se envía a la caja llamada contenedor*/
-			var posteo = document.createTextNode("valor a pagar es $15.000.-";
-			post.appendChild(posteo);
-			result.appendChild(post);	       
-    	}
-     
+ 
+  
